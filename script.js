852da5c0bdcd49d13625183bc5cad8e424808bd3
nandinidas07 <nandinidas107@gmail.com>
2025-07-22 22:01:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-//your JS code here. If required.
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 const buttons = document.querySelectorAll('.btn');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     stopSounds();
+
     const soundName = button.innerText.toLowerCase();
 
     if (soundName === 'stop') return;
 
     const audio = document.getElementById(soundName);
-	  if (audio){
-    audio.play();
-	  }
+    if (audio) {
+      audio.play();
+    }
   });
 });
 
 function stopSounds() {
-	sounds.forEach(sound=>{
-  const audios = document.getElementById(sound);
-  
+  sounds.forEach(sound => {
+    const audio = document.getElementById(sound);
     audio.pause();
     audio.currentTime = 0;
   });
