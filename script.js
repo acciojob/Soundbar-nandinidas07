d4a2217fdf0a1903f1d86c645f896161a9517ef0
nandinidas07 <nandinidas107@gmail.com>
2025-07-22 21:41:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    stopSounds();
+    const soundName = button.innerText.toLowerCase();
+
+    if (soundName === 'stop') return;
+
+    const audio = new Audio(./sounds/${soundName}.mp3);
+    audio.play();
+  });
+});
+
+function stopSounds() {
+  const audios = document.querySelectorAll('audio');
+  audios.forEach(audio => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
